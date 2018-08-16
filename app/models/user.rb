e3c238771a6db841ecc79e8172e6018a51166566
jonaspreisler <jonas.preisler@g>
2018-08-16 20:22:30 +0800

    removed email extension restriction

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,5 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable, :confirmable,
            :recoverable, :rememberable, :trackable, :validatable
     has_one :business
-	validate :email_domain
-
-
-	def email_domain
-		domain = email.split("@").last
-		if !email.blank?
-		  errors.add(:email, "Invalid Domain") if domain != "copona.no" and domain != "jonaspreisler.com"
-		end
-	end
-
 
 end
