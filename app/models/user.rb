2119954d96aa7744d14c7504a658968cb1e3c894
jonaspreisler <jonas.preisler@g>
2018-08-16 20:50:42 +0800

    fdsf

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    devise :database_authenticatable, :registerable, :confirmable,
+    devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :trackable, :validatable
     has_one :business
 
