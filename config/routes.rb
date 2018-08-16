8f443ddc67ece6ccf964ac75992d48311d0ff4d2
jonaspreisler <jonas.preisler@g>
2018-08-16 20:27:33 +0800

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "reservations#new"
   scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
     resources :slots
     resources :reservations
@@ -11,12 +12,12 @@ Rails.application.routes.draw do
     devise_for :users
     resources :businesses
     resources :customers
-    root to: "reservations#new"
     namespace 'api' do
       namespace 'v1' do
         resources :orders
     	end
     end
+
     get '/table', to: 'modals#table'
     get '/dates', to: 'modals#dates'
   end
