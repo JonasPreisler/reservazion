8c6c5a0faa4d3c492e4cdb8c3a23eddbdc9898de
jonaspreisler <jonas.preisler@g>
2018-08-17 10:45:53 +0800

    blabla

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,9 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     'reservations_path' # Or :prefix_to_your_route
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_115607) do
+ActiveRecord::Schema.define(version: 2018_08_17_021432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,6 +152,7 @@ ActiveRecord::Schema.define(version: 2018_08_16_115607) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "secret"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
